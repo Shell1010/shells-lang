6626f655afe540e46a89c41d82074e3b1c825e5e
Shell1010 <amin.dev03@gmail.com>
2024-12-08 22:37:13 +0000

    Parser - omg i think it works

--- src/parser/parser.rs
+++ src/parser/parser.rs
@@ -1,6 +1,6 @@
-use crate::lexer:: Token;
-use crate::lexer::enums::*;
 use super::enums::*;
+use crate::lexer::enums::*;
+use crate::lexer::Token;
 
 #[derive(Clone)]
 pub struct Parser {
@@ -24,78 +24,101 @@ impl Parser {
 
     pub fn parse(&mut self) -> Option<Vec<Statement>> {
         let mut statements = Vec::new();
-        
-        let mut count = 0;
+
         while self.peek().is_some() && self.peek()? != &Token::EndOfInput {
-            count += 1;
             if let Some(statement) = self.parse_statement() {
-                println!("{statement:?}: {count}");
                 statements.push(statement);
             } else {
             }
         }
-        
+
         Some(statements)
     }
 
     fn parse_statement(&mut self) -> Option<Statement> {
         match self.peek()? {
-            Token::Keyword(keyword) => 
-                match keyword.as_str() {
-                    "if" => return self.parse_if_statement(),
-                    _ => None
+            Token::Keyword(keyword) => match keyword.as_str() {
+                "if" => return self.parse_if_statement(),
+                "print" => return self.parse_print_statement(),
+                _ => {
+                    println!("{keyword}");
+                    None
                 }
+            },
             Token::Identifier(_) => self.parse_assignment_or_expression(),
             Token::LeftBrace => self.parse_block(),
-            Token::Comment(ref comment) => {
-                Some(Statement::Comment(comment.clone()))
-            },
-            _ => self.parse_expression().map(Statement::Expression)
+            Token::Comment(_) => self.parse_comment(),
+            _ => self.parse_expression().map(Statement::Expression),
+        }
+    }
+
+    fn parse_comment(&mut self) -> Option<Statement> {
+        if let Some(Token::Comment(val)) = self.peek() {
+            let other_val = val.to_owned();
+            self.advance();
+            println!("{}", self.current);
+            Some(Statement::Comment(other_val))
+        } else {
+            None
         }
     }
 
     fn parse_block(&mut self) -> Option<Statement> {
         if let Some(Token::LeftBrace) = self.advance() {
             let mut statements = Vec::new();
-            
+
             // Parse statements until we hit a right brace
             while let Some(token) = self.peek() {
                 if *token == Token::RightBrace {
                     self.advance(); // consume the right brace
                     break;
                 }
-                
+
                 if let Some(statement) = self.parse_statement() {
                     statements.push(statement);
                 } else {
                     return None;
                 }
             }
-            
+
             Some(Statement::Block(statements))
         } else {
             None
         }
     }
 
+    fn parse_print_statement(&mut self) -> Option<Statement> {
+        if let Some(Token::Keyword(ref keyword)) = self.peek() {
+            if keyword == "print" {
+                self.advance();
+                let condition: Expression = self.parse_expression()?;
+
+                return Some(Statement::Expression(Expression::FunctionCall(
+                    "print".into(),
+                    vec![condition],
+                )));
+            }
+        }
+        None
+    }
+
     fn parse_if_statement(&mut self) -> Option<Statement> {
-        // Consume 'if' keyword
         if let Some(Token::Keyword(ref keyword)) = self.peek() {
             if keyword == "if" {
                 self.advance();
-                
+
                 // Parse condition
                 let condition = self.parse_expression()?;
-                
+
                 // Parse block
                 let body = match self.parse_block()? {
                     Statement::Block(stmts) => stmts,
                     _ => return None,
                 };
-                
+
                 return Some(Statement::Expression(Expression::If(
-                    Box::new(condition), 
-                    body
+                    Box::new(condition),
+                    body,
                 )));
             }
         }
@@ -105,7 +128,7 @@ impl Parser {
     fn parse_assignment_or_expression(&mut self) -> Option<Statement> {
         // Peek at the first token without consuming
         let var = self.peek()?;
-        
+
         if let Token::Identifier(var_name) = var {
             let var_name_other = var_name.to_owned();
             if let Some(next_token) = self.tokens.get(self.current + 1) {
@@ -113,13 +136,13 @@ impl Parser {
                     Token::OperatorToken(Operator::Equals) => {
                         self.advance(); // consume identifier
                         self.advance(); // consume '='
-                        
+
                         let expr = self.parse_expression()?;
                         return Some(Statement::Expression(Expression::Assignment(
                             Box::new(Expression::Identifier(var_name_other)),
-                            Box::new(expr)
+                            Box::new(expr),
                         )));
-                    },
+                    }
                     _ => {
                         let expr = self.parse_expression()?;
                         return Some(Statement::Expression(expr));
@@ -127,7 +150,7 @@ impl Parser {
                 }
             }
         }
-        
+
         None
     }
 
@@ -140,17 +163,13 @@ impl Parser {
 
         while let Some(token) = self.peek() {
             match token {
-                Token::LogicalOperatorToken(op @ LogicalOperator::And) | 
-                Token::LogicalOperatorToken(op @ LogicalOperator::Or) => {
+                Token::LogicalOperatorToken(op @ LogicalOperator::And)
+                | Token::LogicalOperatorToken(op @ LogicalOperator::Or) => {
                     let other_op = op.to_owned();
                     self.advance(); // consume the operator
                     let right = self.parse_bitwise()?;
-                    left = Expression::LogicalOp(
-                        Box::new(left), 
-                        other_op, 
-                        Box::new(right)
-                    );
-                },
+                    left = Expression::LogicalOp(Box::new(left), other_op, Box::new(right));
+                }
                 _ => break,
             }
         }
@@ -162,20 +181,16 @@ impl Parser {
 
         while let Some(token) = self.peek() {
             match token {
-                Token::BitwiseOperatorToken(op @ BitwiseOperator::RightShift) | 
-                Token::BitwiseOperatorToken(op @ BitwiseOperator::LeftShift) | 
-                Token::BitwiseOperatorToken(op @ BitwiseOperator::And) | 
-                Token::BitwiseOperatorToken(op @ BitwiseOperator::Or) | 
-                Token::BitwiseOperatorToken(op @ BitwiseOperator::Xor) => {
+                Token::BitwiseOperatorToken(op @ BitwiseOperator::RightShift)
+                | Token::BitwiseOperatorToken(op @ BitwiseOperator::LeftShift)
+                | Token::BitwiseOperatorToken(op @ BitwiseOperator::And)
+                | Token::BitwiseOperatorToken(op @ BitwiseOperator::Or)
+                | Token::BitwiseOperatorToken(op @ BitwiseOperator::Xor) => {
                     let other_op = op.to_owned();
                     self.advance(); // consume the operator
                     let right = self.parse_comparison()?;
-                    left = Expression::BitwiseOp(
-                        Box::new(left), 
-                        other_op, 
-                        Box::new(right)
-                    );
-                },
+                    left = Expression::BitwiseOp(Box::new(left), other_op, Box::new(right));
+                }
                 _ => break,
             }
         }
@@ -187,21 +202,17 @@ impl Parser {
 
         if let Some(token) = self.peek() {
             match token {
-                Token::ComparisonOperatorToken(op @ ComparisonOperator::GreaterThan) | 
-                Token::ComparisonOperatorToken(op @ ComparisonOperator::LessThan) | 
-                Token::ComparisonOperatorToken(op @ ComparisonOperator::Equals) | 
-                Token::ComparisonOperatorToken(op @ ComparisonOperator::NotEquals) | 
-                Token::ComparisonOperatorToken(op @ ComparisonOperator::GreaterThanEq) | 
-                Token::ComparisonOperatorToken(op @ ComparisonOperator::LessThanEq) => {
+                Token::ComparisonOperatorToken(op @ ComparisonOperator::GreaterThan)
+                | Token::ComparisonOperatorToken(op @ ComparisonOperator::LessThan)
+                | Token::ComparisonOperatorToken(op @ ComparisonOperator::Equals)
+                | Token::ComparisonOperatorToken(op @ ComparisonOperator::NotEquals)
+                | Token::ComparisonOperatorToken(op @ ComparisonOperator::GreaterThanEq)
+                | Token::ComparisonOperatorToken(op @ ComparisonOperator::LessThanEq) => {
                     let other_op = op.to_owned();
                     self.advance(); // consume the operator
                     let right = self.parse_math()?;
-                    left = Expression::ComparisonOp(
-                        Box::new(left), 
-                        other_op, 
-                        Box::new(right)
-                    );
-                },
+                    left = Expression::ComparisonOp(Box::new(left), other_op, Box::new(right));
+                }
                 _ => {}
             }
         }
@@ -210,23 +221,19 @@ impl Parser {
 
     fn parse_math(&mut self) -> Option<Expression> {
         let mut left = self.parse_factor()?;
-        
+
         while let Some(token) = self.peek() {
             match token {
-                Token::MathOperatorToken(op @ MathOperator::Add) | 
-                Token::MathOperatorToken(op @ MathOperator::Subtract) | 
-                Token::MathOperatorToken(op @ MathOperator::Multiply) | 
-                Token::MathOperatorToken(op @ MathOperator::Divide) | 
-                Token::MathOperatorToken(op @ MathOperator::Modulus) => {
+                Token::MathOperatorToken(op @ MathOperator::Add)
+                | Token::MathOperatorToken(op @ MathOperator::Subtract)
+                | Token::MathOperatorToken(op @ MathOperator::Multiply)
+                | Token::MathOperatorToken(op @ MathOperator::Divide)
+                | Token::MathOperatorToken(op @ MathOperator::Modulus) => {
                     let other_op = op.to_owned();
                     self.advance(); // consume the operator
                     let right = self.parse_factor()?;
-                    left = Expression::MathOp(
-                        Box::new(left), 
-                        other_op, 
-                        Box::new(right)
-                    );
-                },
+                    left = Expression::MathOp(Box::new(left), other_op, Box::new(right));
+                }
                 _ => break,
             }
         }
@@ -241,13 +248,11 @@ impl Parser {
             Token::LeftParen => {
                 let expr = self.parse_expression()?;
                 if self.advance()? != &Token::RightParen {
-                    return None;  // expect closing parenthesis
+                    return None; // expect closing parenthesis
                 }
                 Some(Expression::Grouping(Box::new(expr)))
-            },
+            }
             _ => None,
         }
     }
 }
-
-
